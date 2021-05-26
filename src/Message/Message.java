5dc640dff8ec31038097e00920d0cf04ca35c681
Ahmet <ahmetzumber5@gmail.com>
2021-05-26 20:57:23 +0300

    message object created

---
+++ src/Message/Message.java
@@ -0,0 +1,19 @@
+package Message;
+
+public class Message implements java.io.Serializable {
+    //mesaj tipleri enum 
+
+    public static enum Message_Type {
+        Name, LIST, RivalConnected, CHANGE, START_CHAT, GameControl, FINISH
+    }
+
+    //mesajın tipi
+    public Message_Type type;
+
+    //mesajın içeriği obje tipinde ki istenilen tip içerik yüklenebilsin
+    public Object content;
+    
+    public Message(Message_Type t) {
+        this.type = t;
+    }
+}
