f567473ff4a5a73ecec731c81c7298d6b3e66044
Ahmet <ahmetzumber5@gmail.com>
2021-05-31 12:33:45 +0300

    senderName and whoWantsToTalk properties added

--- src/Message/Message.java
+++ src/Message/Message.java
@@ -3,12 +3,18 @@ package Message;
 public class Message implements java.io.Serializable {
 
     public static enum Message_Type {
-        NAME, ROOM_NAME, ROOM_LIST, LIST, JOIN_ROOM, START_CHAT, REFRESH, TEXT
+        NAME, ROOM_NAME, ROOM_LIST, LIST, JOIN_ROOM, START_CHAT, DECIDE, DECIDE_FINISH, TEXT, REFRESH
     }
 
     // type of message
     public Message_Type type;
 
+    // message sender 
+    public String senderName;
+    
+    // who wants to talk
+    public String whoWantsToTalk;
+    
     // casteble message content 
     public Object content;
     
