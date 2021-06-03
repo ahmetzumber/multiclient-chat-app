4a1fbc0180d14a0427cf1ab262ab315d490c5060
Ahmet <ahmetzumber5@gmail.com>
2021-06-03 20:36:50 +0300

    message types modified and file content property added

--- src/Message/Message.java
+++ src/Message/Message.java
@@ -3,8 +3,9 @@ package Message;
 public class Message implements java.io.Serializable {
 
     public static enum Message_Type {
-        NAME, ROOM_NAME, ROOM_LIST, LIST, JOIN_ROOM, START_CHAT, 
-        DECIDE, DECIDE_FINISH, P2P_TEXT, TEXT, REFRESH
+        NAME, ROOM_NAME, ROOM_LIST, LIST,                                       // MENU MESSAGE TYPES
+        JOIN_ROOM, REFRESH, TEXT,  ROOM_FILE, ROOM_FILE_NOTIFY,                 // ROOM MESSAGE TYPES
+        START_CHAT, DECIDE, DECIDE_FINISH, P2P_TEXT, P2P_FILE, P2P_FILE_NOTIFY, // P2P MESSAGE TYPES 
     }
 
     // type of message
@@ -15,6 +16,9 @@ public class Message implements java.io.Serializable {
     
     // who wants to talk
     public String whoWantsToTalk;
+    
+    // to send a file content as a byte array
+    public byte[] fileContent;
     
     // casteble message content 
     public Object content;
